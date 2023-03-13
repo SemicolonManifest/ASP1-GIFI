626483f61c7f7bf74e23728eff6517d59f300ab0
SemicolonManifest <theo.gautier@cpnv.ch>
2023-03-13 16:23:37 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+teacher_user = User.create!(first_name: "Pascal", last_name: "Hurni", email: "pascal.hurni@cpnv.local")
+teacher = Teacher.create!(user: teacher_user);
+
+student_user = User.create!(first_name: "Théo", last_name: "Gautier", email: "theo.gautier@cpnv.local")
+student = Student.create!(user: student_user);
+
+category = Category.create!(name: "Modules de développement")
+
+lecture = Lecture.create!(name: "POO1", category: category, teacher: teacher)
+
+promotion = Promotion.create!(name: "SI-t1a 21-23", teacher: teacher, start_date: Date.new(2021, 9, 1), end_date: Date.new(2023, 6, 30))
+
+
+
